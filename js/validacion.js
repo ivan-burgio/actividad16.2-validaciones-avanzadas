b31a1f47adbf1b7fb03fbc307274eee7c3b62b87
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-19 12:30:31 -0300

    Pronto todo

--- js/validacion.js
+++ js/validacion.js
@@ -3,31 +3,39 @@
 
     const password1 = document.getElementById('password1');
     const password2 = document.getElementById('password2');
+    const termsCheckbox = document.getElementById('terminos');
+    const termsError = document.getElementById('terminosError');
+    const termsAlert = document.getElementById('terminosAlert'); // Nuevo elemento para mostrar el mensaje de alerta
 
-    password2.addEventListener("input", function () {
+    password2.addEventListener('input', function () {
         // Confirmación de contraseña
         if (password1.value !== password2.value) {
-            password2.classList.remove("is-valid");
-            password2.classList.add("is-invalid");
             password2.setCustomValidity('Las contraseñas no coinciden');
         } else {
-            password2.classList.remove("is-invalid");
-            password2.classList.add("is-valid");
             password2.setCustomValidity('');
         }
     });
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
+    // Selección de todos los formularios con la clase 'needs-validation'
+    const forms = document.querySelectorAll('.needs-validation');
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
+    forms.forEach(function (form) {
         form.addEventListener('submit', function (event) {
             if (!form.checkValidity()) {
                 event.preventDefault();
                 event.stopPropagation();
             }
+
+            // Verifica si los términos y servicios están aceptados
+            if (!termsCheckbox.checked) {
+                termsError.style.display = 'block'; // Muestra el mensaje de error
+                termsAlert.style.display = 'block'; // Muestra el mensaje de alerta
+            } else {
+                termsError.style.display = 'none'; // Oculta el mensaje de error
+                termsAlert.style.display = 'none'; // Oculta el mensaje de alerta
+            }
+
             form.classList.add('was-validated');
-        }, false);
+        });
     });
 })();
